791c21b146046b111e4bf95b708c4f8c8194506a
Jonas Rodrigues <jxonas@gmail.com>
2016-04-04 22:15:31 -0300

    rebuilding site Seg  4 Abr 2016 22:15:31 BRT

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:jxonas/terapeuta-oriental.git gh-pages
+git subtree push --prefix=public git@github.com:timulato/timulato.github.io.git gh-pages
