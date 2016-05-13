d928a5e5576b6913dd602aa8d3f5611689913b57
Jonas Rodrigues <jxonas@gmail.com>
2016-05-12 21:28:09 -0300

    rebuilding site Qui 12 Mai 2016 21:28:09 BRT

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin website
+git push origin website --force
 git subtree push --prefix=public git@github.com:timulato/timulato.github.io.git master --force
